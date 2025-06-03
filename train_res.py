2154a976728f3b9ff4f735e8e2311668f4ef0f7f
LittlePrinceCJUer <wuyongjiang2004@outlook.com>
2025-06-03 10:40:47 -0700

    mnist trained

--- train_res.py
+++ train_res.py
@@ -10,10 +10,10 @@ from models.cifar10_res import CIFAR10_ResNet
 from training.utils import train_one_epoch, evaluate
 
 # configs
-DATASET = "cifar10"    # choose "mnist" or "cifar10"
+DATASET = "mnist"    # choose "mnist" or "cifar10"
 BATCH_SIZE = 64       # mini-batch size
-EPOCHS = 300           # number of training epochs
-LR = 0.002            # learning rate for Adam optimizer
+EPOCHS = 5           # number of training epochs
+LR = 0.01            # learning rate for Adam optimizer
 
 # cpu/gpu setup
 device = torch.device(
